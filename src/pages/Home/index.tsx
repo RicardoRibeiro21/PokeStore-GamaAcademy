a0f76c5b7b4f6049355395be5f5819a7d5c17c2c
Ricardo Ribeiro Lopes <ricardo.sao@hotmail.com>
2020-07-08 23:34:57 -0300

    Finalizando projeto

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,9 +16,10 @@ interface Pokemon {
 }
 
 export default function Home() {
-    const [items, setItems] = useState([]);
     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-    const [selectedItems, setSelectedItems] = useState<number[]>([]);
+    const [pokemonsSelected, setPokemonsSelected] = useState<Pokemon[]>([]);
+    const [priceTotal, setPriceTotal] = useState<number>();
+    const [sizeKart, setSizeKart] = useState<number>(Number);
     const dados: Pokemon[] = [];
     const [search, setSearch] = useState<String>(String);
 
@@ -27,11 +28,11 @@ export default function Home() {
             .then((response) => response.data.results.map((item: PokemonBase) => {
                 api.get(`/${item.name}`).then((pokemon => {
                     const objPokemon = {
-                        name: pokemon.data.forms[0].name,
+                        name: pokemon.data.forms[0].name.charAt(0).toUpperCase() + pokemon.data.forms[0].name.slice(1),
                         url: pokemon.data.forms[0].url,
                         img: pokemon.data.sprites.front_default,
                         weight: pokemon.data.weight,
-                        price: String(((pokemon.data.weight / 10 ) * 3 / 2).toFixed(2))
+                        price: String(((pokemon.data.weight / 10) * 3 / 2).toFixed(2))
                     }
                     dados.push(objPokemon)
                     setPokemons([...dados]);
@@ -53,48 +54,79 @@ export default function Home() {
         setSearch(value);
     }
 
-    function handleSelectedItem(id: number) {
+    function handleSelectedItem(pokemon: Pokemon) {
         // Verificando se o item já foi selecionado 
-        const alreadySelected = selectedItems.findIndex(item => item === id)
+        const alreadySelected = pokemonsSelected.findIndex(item => item === pokemon)
+        console.log(alreadySelected);
         if (alreadySelected >= 0) {
-            const filteredItems = selectedItems.filter(item => item !== id);
+            const filteredPokes = pokemonsSelected.filter(item => item !== pokemon)
+            setSizeKart(sizeKart - 1);
 
-            setSelectedItems(filteredItems);
+            setPokemonsSelected(filteredPokes);
         } else {
-            // Mantendo os items que já estão selecionados e passando um novo item
-            setSelectedItems([...selectedItems, id]);
+            // Mantendo os items que já estão selecionados e passando um novo item            
+            setPokemonsSelected([...pokemonsSelected, pokemon]);
+            setSizeKart(sizeKart + 1);
         }
     }
 
+    useEffect(() => {
+        let price: number = 0.00;
+        pokemonsSelected.map(item => price += Number(item.price))
+        setPriceTotal(price);
+        console.log(priceTotal);
+    }, [pokemonsSelected])
+
     return (
         <S.Body>
-            <div className="menu">
-                <S.Input onChange={handleInputChange} />                
-            </div>
+            <S.Menu>
+                <S.Input onChange={handleInputChange} placeholder="Digite o nome do pokémon" />
+            </S.Menu>
             <S.Content>
                 <S.ContainerCards>
                     {
                         pokemons.map((pokemon, index) => {
                             return (
-                                <S.Card key={index}>                                    
-                                    {/* <p>{pokemon.url}</p> */}
-                                    <S.Img src={`${pokemon.img} `}></S.Img>
+                                <S.Card key={index}>
+                                    <S.Img src={pokemon.img}></S.Img>
                                     <p>{pokemon.name}</p>
                                     <p>Peso {pokemon.weight / 10} Kg </p>
-                                    <p>Preço R$ {pokemon.price}</p>
-                                    <S.Button onClick={() => handleSelectedItem(index)}> Adicionar ao Carrinho </S.Button>
+                                    <p>R$ {pokemon.price}</p>
+                                    <S.Button onClick={() => handleSelectedItem(pokemon)}>Adicionar ao carrinho</S.Button>
                                 </S.Card>
                             )
                         })
                     }
                 </S.ContainerCards>
                 <S.Carrinho>
-                    <h1> Teste do carrinho</h1>
-                    {
-                        selectedItems.map(item => {
-                            return <p>{item}</p>
-                        })
-                    }
+                    <div id="carrinho">
+                    </div>
+                    <h1> Carrinho</h1>
+                    <S.Table>
+                        {/* <thead>
+                            <S.TableColumn></S.TableColumn>
+                            <S.TableColumn>Nome</S.TableColumn>
+                            <S.TableColumn>Preço</S.TableColumn>
+                        </thead> */}
+                        <tbody>
+                        </tbody>
+                        {
+                            pokemonsSelected.map((pokemon, index) => {
+                                return (
+                                    <S.TableRow>
+                                        <S.TableColumn><S.Img src={pokemon.img}></S.Img></S.TableColumn>
+                                        <S.TableColumn><p>{pokemon.name}</p></S.TableColumn>
+                                        <S.TableColumn><p>R$ {pokemon.price}</p></S.TableColumn>
+                                    </S.TableRow>
+                                )
+                            })
+                        }
+                    </S.Table>
+                    <S.CarrinhoFooter>
+                        <p>Quantidade {sizeKart}</p>
+                        <p>Total R$ {String(priceTotal?.toFixed(2))}</p>
+                        <S.ButtonStore>Finalizar Compra</S.ButtonStore>
+                    </S.CarrinhoFooter>
                 </S.Carrinho>
             </S.Content>
         </S.Body>
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -1,14 +1,21 @@
 import styled from 'styled-components';
 
+export const Menu = styled.div`
+    width: 90%;
+    height: 3rem;
+    margin-top: 0;
+    background: #333;
+    dispay: flex;        
+`
+
 // Carrinho 
 export const Input = styled.input`
-height: 1.5rem;
-padding: 0 .5rem; 
-border: 1px solid #ddd;
-border-radius: .25rem 0 0 .25rem;
-margin: 8% 0;
-&:focus,
-&:active {
+    height: 1.5rem;    
+    border: 1px solid #ddd;
+    border-radius: .25rem 0 0 .25rem;
+    margin: .5rem 10%;
+    &:focus,
+    &:active {
     outline: none;
     box-shadow: none;
 }
@@ -39,14 +46,26 @@ export const ContainerCards = styled.div`
 `;
 
 export const Card = styled.div`
-    margin: 0 5%;
+    margin: 5% 5%;
+    padding: .5rem;
     border-bottom: #EB2049 groove 2px;
+    border-top-left-radius: .5rem;
+    border-top-right-radius: .5rem;
     &:hover {
-        opacity: 75%;        
+        opacity: 75%; 
+        transition: 0.5s;
+        background: #333; 
+        color: white;                 
+        border-bottom: #333 groove 2px;
+        button {
+            background: #EB2049;
+            border: #EB2049;
+        }
     };
     &:focus {
         background: #333;
-    }
+    };
+    
 `;
 
 export const Img = styled.img`
@@ -58,21 +77,92 @@ export const Button = styled.button`
     border: 1px solid #333;
     border-radius: .25rem ;
     margin: 5% 15% 10%;
-    height: 1.8rem;    
+    height: 2rem;    
     color: #fff;
     background: #333;
     cursor: pointer;
-    &:focus {
-        cursor: pointer;
-    };
+    // &:active {
+    //     outline: none;
+    //     box-shadow: none;        
+    // }
+`;
+
+export const ButtonStore = styled.button`
+    border: 1px solid #333;
+    border-radius: .25rem ;    
+    height: 2rem;    
+    color: #fff;
+    background: #EB2049;
+    cursor: pointer;
     &:active {
         outline: none;
-        box-shadow: none;        
-    }
+        box-shadow: none;                
+    };
+    &:hover {
+        transition: 0.2s;
+        height: 2.5rem;
+        width: 35%;
+    };
 `;
 
 // Estilizações do Carrinho
 export const Carrinho = styled.div`
+    position: relative;    
     width: 25%;
-    background-color: rgba(24,251,110,0.7);    
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    height: 400px;    
+    margin-left: 5%;
+    padding: 5px;
+    border: 2px groove rgba(255, 0, 0, .35);
+    border-radius: .5rem;
+    text-align: center;        
+`;
+
+export const Table = styled.table`
+   width: 100%;   
+   height: 250px;
+   overflow-y: scroll;
+   ::-webkit-scrollbar {
+       border-radius: .5rem;
+       width: 12px;
+       background: #EBF0F6;
+   };
+   ::-webkit-scrollbar-thumb {
+       border-radius: .5rem;
+       background: #EB7075;
+   };
+   img {
+       height: 120%;
+       width: 70%;
+   },
+   thead, tbody{
+       display: block;  
+    }
+`;
+
+export const TableRow = styled.tr`
+    cursor: pointer;    
+    &:hover {
+        opacity: 75%;        
+        background: #333;
+        color: #fff;
+    };
+`;
+
+export const TableColumn = styled.td`
+   width: 41%;
+`;
+
+export const CarrinhoFooter = styled.div`
+   display: flex;
+   width: 90%;
+   margin: 5% 0;
+   align-items:center;
+   background: red;
+   color: #fff;
+   background: #333;
+   justify-content: space-around;
+   border-radius: .5rem;
 `;
